e3e725eacb17677cc4fdcf4b66536ba1ad4ef8b7
Erki <erkiparna7@gmail.com>
2022-09-20 01:28:40 -0700

    Variables

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
 let name = 'Erki P2rna'
 name = 'New name'
+console.log(typeof name)
+
+let goodmark = 5
+console.log(typeof goodmark)
+
+let persent = 50.3
+console.log(typeof persent)
+
+let isHere = true
+console.log(typeof isHere)
 
 const surname = 'P2rna'
 // surname = 'P2rn' - error
@@ -16,3 +26,14 @@ const person = {
 
 person.department = "IKT"
 console.log(person)
+console.log(typeof person)
+
+const airplane = null
+console.log(typeof airplane)
+
+const symbol = Symbol()
+console.log(typeof symbol)
+
+let test
+console.log(test)
+console.log(typeof test)
