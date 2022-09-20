1555b94ea3f7cb82e31a0a674c8768a7a8904bd1
Erki <erkiparna7@gmail.com>
2022-09-20 01:18:22 -0700

    Variables

---
+++ app.js
@@ -0,0 +1,18 @@
+let name = 'Erki P2rna'
+name = 'New name'
+
+const surname = 'P2rna'
+// surname = 'P2rn' - error
+
+const numbers = [1, 2, 3, 4, 5]
+console.log(numbers)
+numbers.push(6)
+console.log(numbers)
+
+const person = {
+    firstname: 'Erki',
+    surname: 'P2rna'
+}
+
+person.department = "IKT"
+console.log(person)
