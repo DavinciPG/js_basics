e8943e1fab97fc4c36cb39e0e58a5188fe59b175
Erki <erkiparna7@gmail.com>
2022-09-20 23:28:39 -0700

    Variables

--- app.js
+++ app.js
@@ -1,25 +1,22 @@
 const firstname = 'Erki'
 const surname = 'P2rna'
+const email = 'erki.parna@voco.ee'
+const department = 'IKT'
 
-let val = firstname + ' ' + surname
-val = 'Erki '
-val+= surname
+let result = '<ul>' +
+    '<li><b>Name: </b>' + firstname + '</li>' +
+    '<li><b>Surname: </b>' + surname + '</li>' +
+    '<li><b>Email: </b>' + email + '</li>' +
+    '<li><b>Department: </b>' + department + '</li>' +
+    '</ul>'
 
-val = firstname.toUpperCase()
-val = surname.toLowerCase()
+result = `
+<ul>
+    <li><b>Name: </b>${firstname}</li>
+    <li><b>Surname: </b>${surname}</li>
+    <li><b>Email: </b>${email}</li>
+    <li><b>Department: </b>${department}</li>
+</ul>`
 
-val = surname[0]
-val = surname.indexOf('E')
-val = surname.lastIndexOf('i')
-
-val = surname.charAt(2)
-val = surname.charAt(surname.length - 1)
-
-val = surname.substring(0, 4)
-val = surname.slice(0, 4)
-val = surname.slice(-3)
-
-const tags = 'JS, HTML, CSS'
-val = tags.split(',')
-
-console.log(val[1].trim())
+console.log(result)
+document.body.innerHTML = result
